ab2984d6c123aef8413e7de9d9f3a4ff8d196e60
Daniel Ledda <ledda@cip.ifi.lmu.de>
2021-09-04 09:18:28 +0200

    chore: hook-test

--- src/ui/BeatGroup/Beat/BeatView.ts
+++ src/ui/BeatGroup/Beat/BeatView.ts
@@ -6,7 +6,6 @@ import ISubscriber from "../../../Subscriber";
 import BeatUnitView from "./BeatUnit/BeatUnitView";
 import "./Beat.css";
 
-//some
 export type BeatUINodeOptions = UINodeOptions & {
     beat: Beat,
 };
