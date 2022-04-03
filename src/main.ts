26a338121feb3b3ef5803626e382916aa96cada6
Daniel Ledda <ledda@cip.ifi.lmu.de>
2022-04-03 12:27:26 +0200

    chore: testing new hook

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ if (appNode) {
     try {
         const appRoot = new RootView({
             orientation: "vertical",
-            title: "OBVIOUS CHANGING",
+            title: "OBVIOUS CHANGES",
         });
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         //@ts-ignore
