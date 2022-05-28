ebe322dd15c3a9ae3872b3a58db419f65c534087
Daniel Ledda <ledda@cip.ifi.lmu.de>
2022-05-28 19:40:07 +0200

    refactor: used bootstrap method from ladder

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,15 @@
 import RootView from "@/ui/Root/RootView";
 import "@/ui/global.css";
+import { bootstrap } from "@djledda/ladder";
 
-const appNode = document.querySelector("#app");
-
-if (appNode) {
-    try {
-        const appRoot = new RootView({
-            orientation: "vertical",
-            title: "Drum Slayer",
-        });
-        window.appRoot = appRoot;
-        appNode.appendChild(appRoot.render());
-        console.log("OK!");
-    } catch (e) {
-        console.error("FUCK!", e);
-    }
-} else {
-    console.error("FUCK!");
+try {
+    const appRoot = new RootView({
+        orientation: "vertical",
+        title: "Drum Slayer",
+    });
+    window.appRoot = appRoot;
+    bootstrap(appRoot, "app");
+    console.log("OK!");
+} catch (e) {
+    console.error("FUCK!", e);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "vite";
 export default defineConfig({
     resolve: {
         alias: {
-            '@': '/src',
-            'assets': '/assets',
+            "@": "/src",
+            "assets": "/assets",
         }
     },
 });
